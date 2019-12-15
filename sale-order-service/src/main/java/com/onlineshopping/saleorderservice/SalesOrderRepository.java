a77248ec3bc2f35faddc98f1ef4ec14f42d2b66a
student <student@win2012base>
2019-12-15 01:02:03 -0800

    version 1 code deleted

--- sale-order-service/src/main/java/com/onlineshopping/saleorderservice/SalesOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onlineshopping.saleorderservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalesOrderRepository extends JpaRepository<Order, Long> {
-
-}
