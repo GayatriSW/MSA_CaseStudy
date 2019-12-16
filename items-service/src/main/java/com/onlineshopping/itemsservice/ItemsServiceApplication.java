250365f3d7ebc5ab4d21409c251f9178a3c10f54
student <student@win2012base>
2019-12-16 03:39:13 -0800

    Version 3 - Added SCS dependencies - config client, service registry.

--- items-service/src/main/java/com/onlineshopping/itemsservice/ItemsServiceApplication.java
+++ items-service/src/main/java/com/onlineshopping/itemsservice/ItemsServiceApplication.java
@@ -1,10 +1,13 @@
 package com.onlineshopping.itemsservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {ManagementWebSecurityAutoConfiguration.class,
+		SecurityAutoConfiguration.class})
 @EnableDiscoveryClient
 public class ItemsServiceApplication {
 
