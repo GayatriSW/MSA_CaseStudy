3857471895f3203be57f76dbc5bd6d1e1f823c58
student <student@win2012base>
2019-12-16 08:22:23 -0800

    version 3.4 - disabling security in custtomer & sales MS

--- sale-order-service/src/main/java/com/onlineshopping/saleorderservice/SaleOrderServiceApplication.java
+++ sale-order-service/src/main/java/com/onlineshopping/saleorderservice/SaleOrderServiceApplication.java
@@ -1,11 +1,14 @@
 package com.onlineshopping.saleorderservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {ManagementWebSecurityAutoConfiguration.class,
+		SecurityAutoConfiguration.class})
 @EnableFeignClients("com.onlineshopping.saleorderservice")
 @EnableDiscoveryClient
 public class SaleOrderServiceApplication {
